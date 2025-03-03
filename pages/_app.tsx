076053913c2506432d6511b2a56a24dce4920042
Arnob Mahmud <arnob_t78@yahoo.com>
2025-03-03 23:54:53 +0100

    fix deploy bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { AppProps } from "next/app"; // Import AppProps for typing
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   // Use AppProps for typing
   return (
     <AuthProvider>
