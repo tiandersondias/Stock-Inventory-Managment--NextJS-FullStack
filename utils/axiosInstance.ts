e1d2f0ad9a1265ba9f7d33e7abc86fab415470a6
Arnob Mahmud <arnob_t78@yahoo.com>
2025-04-06 20:57:39 +0200

    vercel deploy

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,9 +1,31 @@
+// import axios from "axios";
+// import Cookies from "js-cookie";
+
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:3000/api", // Ensure this points to the correct base URL
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// axiosInstance.interceptors.request.use((config) => {
+//   const token = Cookies.get("session_id");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default axiosInstance;
+
 import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api",
-  // Ensure this points to the correct base URL
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://stockly-inventory.vercel.app/api" // Production URL
+      : "http://localhost:3000/api", // Localhost for development
   headers: {
     "Content-Type": "application/json",
   },
