c4b0db79199e5cec8aedb960973dec354b26270a
Arnob Mahmud <arnob_t78@yahoo.com>
2025-04-06 18:10:12 +0200

    vercel deploy fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,63 @@
+// "use client";
+
+// import React, { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
+// import Home from "./Home";
+// import Login from "./login/page";
+// import { useAuth } from "./authContext";
+// import Loading from "../components/Loading";
+
+// interface PageProps {
+//   params: Promise<{ [key: string]: any }>;
+//   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+// }
+
+// const PageContent: React.FC<{ onLogin: () => void }> = ({ onLogin }) => {
+//   const router = useRouter();
+//   const { isLoggedIn } = useAuth(); // Use isLoggedIn from useAuth
+
+//   useEffect(() => {
+//     if (!isLoggedIn) {
+//       router.push("/login");
+//     }
+//   }, [isLoggedIn, router]);
+
+//   if (isLoggedIn) {
+//     return <Home />;
+//   }
+
+//   return <Login onLogin={onLogin} />;
+// };
+
+// const Page: React.FC<PageProps> = ({ params, searchParams }) => {
+//   const [resolvedParams, setResolvedParams] = useState<{
+//     [key: string]: any;
+//   } | null>(null);
+//   const [resolvedSearchParams, setResolvedSearchParams] = useState<{
+//     [key: string]: string | string[] | undefined;
+//   } | null>(null);
+
+//   useEffect(() => {
+//     const resolveParams = async () => {
+//       setResolvedParams(await params);
+//       setResolvedSearchParams(await searchParams);
+//     };
+//     resolveParams();
+//   }, [params, searchParams]);
+
+//   const handleLogin = () => {
+//     console.log("User logged in");
+//   };
+
+//   if (!resolvedParams || !resolvedSearchParams) {
+//     return <Loading />;
+//   }
+
+//   return <PageContent onLogin={handleLogin} />;
+// };
+
+// export default Page;
+
 "use client";
 
 import React, { useEffect, useState } from "react";
@@ -12,7 +72,7 @@ interface PageProps {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
-const PageContent: React.FC<{ onLogin: () => void }> = ({ onLogin }) => {
+const PageContent: React.FC = () => {
   const router = useRouter();
   const { isLoggedIn } = useAuth(); // Use isLoggedIn from useAuth
 
@@ -26,7 +86,7 @@ const PageContent: React.FC<{ onLogin: () => void }> = ({ onLogin }) => {
     return <Home />;
   }
 
-  return <Login onLogin={onLogin} />;
+  return <Login />;
 };
 
 const Page: React.FC<PageProps> = ({ params, searchParams }) => {
@@ -45,15 +105,11 @@ const Page: React.FC<PageProps> = ({ params, searchParams }) => {
     resolveParams();
   }, [params, searchParams]);
 
-  const handleLogin = () => {
-    console.log("User logged in");
-  };
-
   if (!resolvedParams || !resolvedSearchParams) {
     return <Loading />;
   }
 
-  return <PageContent onLogin={handleLogin} />;
+  return <PageContent />;
 };
 
 export default Page;
